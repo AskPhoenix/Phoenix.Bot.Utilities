df4b5a689facfa39e9c54ca6eeae7ea4300b6584
Théophile Spyrou <alfatheo21@gmail.com>
2020-11-12 21:41:53 +0200

    Add miscellaneous utilities

---
+++ Phoenix.Bot.Utilities/Miscellaneous/ArithmeticExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Phoenix.Bot.Utilities.Arithmetic
+{
+    public static class ArithmeticExtensions
+    {
+        public static int[] GetDigitsArray(this int value)
+        {
+            var digits = new Stack<int>();
+
+            for (; value > 0; value /= 10)
+                digits.Push(value % 10);
+
+            return digits.ToArray();
+        }
+    }
+}
---
+++ Phoenix.Bot.Utilities/Miscellaneous/CalendarExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Phoenix.Bot.Utilities.Miscellaneous
+{
+    public static class CalendarExtensions
+    {
+        public static DateTime GreeceLocalTime()
+               => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("E. Europe Standard Time"));
+    }
+}
