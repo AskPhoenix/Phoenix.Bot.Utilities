cb65adf038a71335ae7673ca5ce9d7baf00ab049
Théophile Spyrou <alfatheo21@gmail.com>
2020-11-12 21:41:26 +0200

    Extend linguistic utilities

---
+++ Phoenix.Bot.Utilities/Linguistic/Greek.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phoenix.Bot.Utilities.Linguistic
+{
+    public static class Greek
+    {
+        public static string NameVocative(string name)
+        {
+            if (!name.EndsWith('ς'))
+                return name;
+
+            char letter = name.Substring(name.Length - 2, 1).ToCharArray()[0];
+
+            if (letter == 'ο')
+                letter = 'ε';
+            else if (letter == 'ό')
+                letter = 'έ';
+
+            return name.Substring(0, name.Length - 2) + letter;
+        }
+
+        public static string DayArticle(DayOfWeek day, bool accusative = true)
+        {
+            if (accusative)
+            {
+                if (day == DayOfWeek.Monday)
+                    return "τη";
+                else if (day == DayOfWeek.Saturday)
+                    return "το";
+                else
+                    return "την";
+            }
+            else
+            {
+                if (day == DayOfWeek.Saturday)
+                    return "το";
+                else
+                    return "η";
+            }
+        }
+    }
+}
---
+++ Phoenix.Bot.Utilities/Linguistic/Normalization.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Phoenix.Bot.Utilities.Linguistic
+{
+    public static class Normalization
+    {
+        public static string ToUnaccented(this string str)
+        {
+            string fullCanonicalDecompositionNormalized = str.Normalize(NormalizationForm.FormD);
+            var unaccented = fullCanonicalDecompositionNormalized.Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark);
+
+            return new string(unaccented.ToArray());
+        }
+
+        public static string TrimEmojis(this string str)
+            => new string(str.Where(c => !char.IsSurrogate(c) && !char.IsSymbol(c)).ToArray()).Trim();
+    }
+}
