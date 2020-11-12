320e1d1a930a4fe56b120eb1eb08a24896eef7a4
Théophile Spyrou <alfatheo21@gmail.com>
2020-11-12 20:00:18 +0200

    Add AdaptiveCards utilities

---
+++ Phoenix.Bot.Utilities/AdaptiveCards/AdaptiveRichFactSetLight.cs
@@ -0,0 +1,58 @@
+﻿using AdaptiveCards;
+using System.Collections.Generic;
+
+namespace Phoenix.Bot.Utilities.AdaptiveCards
+{
+    public class AdaptiveRichFactSetLight : AdaptiveRichTextBlock
+    {
+        /// <summary>
+        /// Initializes an empty <see cref="AdaptiveRichFactSetLight"/>.
+        /// </summary>
+        public AdaptiveRichFactSetLight() : base()
+        {
+            this.Inlines = new List<AdaptiveInline>()
+                {
+                    new AdaptiveTextRun()
+                    {
+                        Color = AdaptiveTextColor.Light,
+                        Size = AdaptiveTextSize.Large,
+                        Weight = AdaptiveTextWeight.Bolder
+                    },
+                    new AdaptiveTextRun()
+                    {
+                        Color = AdaptiveTextColor.Light,
+                        Size = AdaptiveTextSize.Large,
+                        Weight = AdaptiveTextWeight.Lighter
+                    }
+                };
+        }
+
+        /// <summary>
+        /// Initializes an <see cref="AdaptiveRichFactSetLight"/> instance with the supplied properties.
+        /// </summary>
+        /// <param name="fact">The fact text of the FactSet.</param>
+        /// <param name="value">The value of the FactSet.</param>
+        /// <param name="separator">Include a separator after the FactSet, or not.</param>
+        public AdaptiveRichFactSetLight(string fact, string value, bool separator = false) : base()
+        {
+            this.Inlines = new List<AdaptiveInline>()
+                {
+                    new AdaptiveTextRun()
+                    {
+                        Text = fact,
+                        Color = AdaptiveTextColor.Light,
+                        Size = AdaptiveTextSize.Large,
+                        Weight = AdaptiveTextWeight.Bolder
+                    },
+                    new AdaptiveTextRun()
+                    {
+                        Text = value,
+                        Color = AdaptiveTextColor.Light,
+                        Size = AdaptiveTextSize.Large,
+                        Weight = AdaptiveTextWeight.Lighter
+                    }
+                };
+            this.Separator = separator;
+        }
+    }
+}
---
+++ Phoenix.Bot.Utilities/AdaptiveCards/AdaptiveTextBlockHeaderLight.cs
@@ -0,0 +1,25 @@
+﻿using AdaptiveCards;
+
+namespace Phoenix.Bot.Utilities.AdaptiveCards
+{
+    public class AdaptiveTextBlockHeaderLight : AdaptiveTextBlock
+    {
+        /// <summary>
+        /// Initializes an empty <see cref="AdaptiveTextBlockHeaderLight"/> instance.
+        /// </summary>
+        public AdaptiveTextBlockHeaderLight() : this("") { }
+
+        /// <summary>
+        /// Initializes an <see cref="AdaptiveTextBlockHeaderLight"/> instance with the supplied text.
+        /// </summary>
+        /// <param name="text">The text of this TextBlock.</param>
+        public AdaptiveTextBlockHeaderLight(string text) : base(text)
+        {
+            this.Color = AdaptiveTextColor.Light;
+            this.HorizontalAlignment = AdaptiveHorizontalAlignment.Center;
+            this.Size = AdaptiveTextSize.ExtraLarge;
+            this.Weight = AdaptiveTextWeight.Bolder;
+            this.Wrap = true;
+        }
+    }
+}
