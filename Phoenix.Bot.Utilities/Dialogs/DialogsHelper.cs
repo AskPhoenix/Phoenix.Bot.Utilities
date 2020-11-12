f9a60b37308b921f0bb7edb22c0cf9391e61d9d0
Théophile Spyrou <alfatheo21@gmail.com>
2020-11-12 21:42:11 +0200

    Add Dialogs utilities

---
+++ Phoenix.Bot.Utilities/Dialogs/DialogsHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Phoenix.Bot.Utilities.Miscellaneous;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Phoenix.Bot.Utilities.Dialogs
+{
+    public static class DialogsHelper
+    {
+        public static DateTime ResolveDateTime(IList<DateTimeResolution> dateTimeResolution)
+        {
+            if (dateTimeResolution == null || dateTimeResolution.Count == 0)
+                throw new Exception("Date Time Resolution cannot be null or empty.");
+            if (dateTimeResolution.Count == 1)
+                return DateTime.Parse(dateTimeResolution.Single().Value);
+
+            // The result gives 2 Dates with different year.
+            // If the date is past for the current year, then the results are (1) for the current year and (2) for the previous one.
+            // If the date is not past for the current year, then the results are (1) for the previous year and (2) for the current one.
+
+            var dateTimes = dateTimeResolution.Select(r => DateTime.Parse(r.Value)).OrderBy(d => d.Year);
+            var grDateTime = CalendarExtensions.GreeceLocalTime();
+
+            //bool isPastDate = dateTimes.All(d => d.Month < grDateTime.Month || (d.Month == grDateTime.Month && d.Day == grDateTime.Day));
+            //return isPastDate ? dateTimes.FirstOrDefault(d => d.Year == grDateTime.Year) : dateTimes.LastOrDefault();
+
+            // Just return the closest date including its year.
+            return dateTimes.Aggregate((d, cd) => Math.Abs((d - grDateTime).Days) < Math.Abs((cd - grDateTime).Days) ? d : cd);
+        }
+    }
+}
---
+++ Phoenix.Bot.Utilities/Dialogs/PromptValidators.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Phoenix.Bot.Utilities.Dialogs
+{
+    public static class PromptValidators
+    {
+        public static Task<bool> PhoneNumberPromptValidator(PromptValidatorContext<long> promptContext, CancellationToken _)
+        {
+            return Task.FromResult(
+                promptContext.Recognized.Succeeded &&
+                promptContext.Recognized.Value > 0 &&
+                (Math.Ceiling(Math.Log10(promptContext.Recognized.Value)) == 10 && promptContext.Recognized.Value / 100000000 == 69) ||
+                (Math.Ceiling(Math.Log10(promptContext.Recognized.Value)) == 12 && promptContext.Recognized.Value / 100000000 == 3069));
+        }
+
+        public static Task<bool> PinPromptValidator(PromptValidatorContext<int> promptContext, CancellationToken _)
+        {
+            //TODO: Take TestPin from appsettings
+            int digits = Convert.ToInt32(promptContext.Options.Validations);
+            return Task.FromResult(promptContext.Recognized.Succeeded &&
+                ((int)Math.Ceiling(Math.Log10(promptContext.Recognized.Value)) == digits || promptContext.Recognized.Value == 15422198));
+        }
+    }
+}
