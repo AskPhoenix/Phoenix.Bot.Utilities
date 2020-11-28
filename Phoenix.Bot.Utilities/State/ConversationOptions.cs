b2838a55292d8453836793dc66afa6960df51ae9
Théophile Spyrou <alfatheo21@gmail.com>
2020-11-28 23:50:46 +0200

    Create structures for user and conversation options

---
+++ Phoenix.Bot.Utilities/State/ConversationOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace Phoenix.Bot.Utilities.State
+{
+    public struct ConversationsOptions
+    {
+        public bool NeedsWelcoming { get; set; }
+        public int Page { get; set; }
+        public string Locale { get; set; }
+
+        public AuthenticationOtions Authentication { get; set; }
+        public CourseOptions Course { get; set; }
+
+        public struct AuthenticationOtions
+        {
+            public string PhoneNumber { get; set; }
+            public string OneTimeCode { get; set; }
+        }
+
+        public struct CourseOptions
+        {
+            public int[] SelectedCourseIds { get; set; }
+            public int EnrolledCoursesCount { get; set; }
+        }
+    }
+}
---
+++ Phoenix.Bot.Utilities/State/UserOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Phoenix.Bot.Utilities.State
+{
+    public struct UserOptions
+    {
+        public bool IsAuthenticated { get; set; }
+        public bool HasAcceptedTerms { get; set; }
+        public int SmsUsed { get; set; }
+        public int Role { get; set; }
+    }
+}
