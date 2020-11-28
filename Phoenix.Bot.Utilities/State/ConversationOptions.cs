4fd4b0a4e5f1922697e1a735d5011dbd96a361d5
Théophile Spyrou <alfatheo21@gmail.com>
2020-11-29 00:31:44 +0200

    Edit Conversation Option structures

--- Phoenix.Bot.Utilities/State/ConversationOptions.cs
+++ Phoenix.Bot.Utilities/State/ConversationOptions.cs
@@ -6,19 +6,19 @@
         public int Page { get; set; }
         public string Locale { get; set; }
 
-        public AuthenticationOtions Authentication { get; set; }
+        public AuthenticationOptions Authentication { get; set; }
         public CourseOptions Course { get; set; }
+    }
 
-        public struct AuthenticationOtions
-        {
-            public string PhoneNumber { get; set; }
-            public string OneTimeCode { get; set; }
-        }
+    public struct AuthenticationOptions
+    {
+        public string PhoneNumber { get; set; }
+        public string OneTimeCode { get; set; }
+    }
 
-        public struct CourseOptions
-        {
-            public int[] SelectedCourseIds { get; set; }
-            public int EnrolledCoursesCount { get; set; }
-        }
+    public struct CourseOptions
+    {
+        public int[] SelectedCourseIds { get; set; }
+        public int EnrolledCoursesCount { get; set; }
     }
 }
