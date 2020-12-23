db51b003c6f4170431c5ae224cbcc5e9c3f86350
Théophile Spyrou <alfatheo21@gmail.com>
2020-12-23 14:53:33 +0200

    Create PromptNames class

---
+++ Phoenix.Bot.Utilities/Dialogs/PromptNames.cs
@@ -0,0 +1,8 @@
+﻿namespace Phoenix.Bot.Utilities.Dialogs
+{
+    public static class PromptNames
+    {
+        public const string Phone = "Phone_NumberPrompt";
+        public const string Pin = "Pin_NumberPrompt";
+    }
+}
