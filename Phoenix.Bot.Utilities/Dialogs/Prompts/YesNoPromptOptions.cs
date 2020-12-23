c4827e8e93dfcce3b7247f8a51bd672922378137
Théophile Spyrou <alfatheo21@gmail.com>
2020-12-23 16:45:00 +0200

    Create Options for Yes/No Prompt

---
+++ Phoenix.Bot.Utilities/Dialogs/Prompts/YesNoPromptOptions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+
+namespace Phoenix.Bot.Utilities.Dialogs.Prompts
+{
+    public class YesNoPromptOptions : PromptOptions
+    {
+        public YesNoPromptOptions() : base()
+        {
+            this.RetryPrompt = MessageFactory.Text("Παρακαλώ απάντησε με ένα Ναι ή Όχι:");
+            this.Choices = Choices = new Choice[] { new Choice("✔️ Ναι"), new Choice("❌ Όχι") };
+        }
+
+        public YesNoPromptOptions(string promptText) : this()
+        {
+            this.Prompt = MessageFactory.Text(promptText);
+        }
+    }
+}
