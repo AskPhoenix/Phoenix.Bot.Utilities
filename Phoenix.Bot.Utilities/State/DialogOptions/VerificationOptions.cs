a90d83defbddff428175e02a8e7b6caf235412fd
Théophile Spyrou <alfatheo21@gmail.com>
2020-12-28 17:58:17 +0200

    Add Phone option in VerificationOptions

--- Phoenix.Bot.Utilities/State/DialogOptions/VerificationOptions.cs
+++ Phoenix.Bot.Utilities/State/DialogOptions/VerificationOptions.cs
@@ -4,6 +4,7 @@
     {
         public bool IsOwnerVerification { get; set; }
         public int? UserToVerifyId { get; set; }
+        public string Phone { get; set; }
         public bool Verified { get; set; }
     }
 }
