555ffeb2fab1714354c17a892575d5fad474032d
Théophile Spyrou <alfatheo21@gmail.com>
2020-12-28 17:11:34 +0200

    Remove AuthenticationOptions from ConversationOptions

--- Phoenix.Bot.Utilities/State/ConversationOptions.cs
+++ Phoenix.Bot.Utilities/State/ConversationOptions.cs
@@ -1,27 +1,16 @@
-﻿using System;
-
-namespace Phoenix.Bot.Utilities.State
+﻿namespace Phoenix.Bot.Utilities.State
 {
     public struct ConversationsOptions
     {
         public int Page { get; set; }
         public string Locale { get; set; }
 
-        public AuthenticationOptions Authentication { get; set; }
         public CourseOptions Course { get; set; }
     }
 
     public static class ConversationOptionsDefaults
     {
         public const string PropertyName = "Options";
-    }
-
-    public class AuthenticationOptions
-    {
-        public string PhoneNumber { get; set; }
-        public string OTC { get; set; }
-        public bool OTCUsed { get; set; }
-        public DateTimeOffset OTCCreatedAt { get; set; }
     }
 
     public class CourseOptions
