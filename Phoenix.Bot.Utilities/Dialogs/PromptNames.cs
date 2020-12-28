e026d108327da752b9b0cde47de397f716884858
Théophile Spyrou <alfatheo21@gmail.com>
2020-12-28 17:57:35 +0200

    Edit PromptNames

--- Phoenix.Bot.Utilities/Dialogs/PromptNames.cs
+++ Phoenix.Bot.Utilities/Dialogs/PromptNames.cs
@@ -3,6 +3,6 @@
     public static class PromptNames
     {
         public const string Phone = "Phone_NumberPrompt";
-        public const string Pin = "Pin_NumberPrompt";
+        public const string Code = "Code_NumberPrompt";
     }
 }
