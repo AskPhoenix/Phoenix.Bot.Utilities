37cb692206ffca7b14a2b9bc32e5df0e8046135e
Théophile Spyrou <alfatheo21@gmail.com>
2021-01-23 23:38:55 +0200

    Create Options for Student Dialog

---
+++ Phoenix.Bot.Utilities/State/Options/StudentOptions.cs
@@ -0,0 +1,10 @@
+﻿using Phoenix.Bot.Utilities.Actions;
+
+namespace Phoenix.Bot.Utilities.State.Options
+{
+    public class StudentOptions
+    {
+        public int StudentId { get; set; }
+        public StudentAction Action { get; set; }
+    }
+}
