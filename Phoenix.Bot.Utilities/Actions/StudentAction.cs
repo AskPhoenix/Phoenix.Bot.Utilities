680d0ee4ed6e4212831a4567518cb7c5c7365270
Théophile Spyrou <alfatheo21@gmail.com>
2021-01-23 23:38:13 +0200

    Add Actions enum for Students

---
+++ Phoenix.Bot.Utilities/Actions/StudentAction.cs
@@ -0,0 +1,12 @@
+﻿namespace Phoenix.Bot.Utilities.Actions
+{
+    public enum StudentAction
+    {
+        NoAction = 0,
+        Exercises,
+        Exams,
+        Schedule,
+        Help,
+        Feedback
+    }
+}
