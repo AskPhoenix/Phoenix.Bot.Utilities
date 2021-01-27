8d786fe2eae49e228a6215a36246e7c000f60e43
Théophile Spyrou <alfatheo21@gmail.com>
2021-01-27 23:58:29 +0200

    Update Prompt Names

--- Phoenix.Bot.Utilities/Dialogs/PromptNames.cs
+++ Phoenix.Bot.Utilities/Dialogs/PromptNames.cs
@@ -1,8 +1,12 @@
-﻿namespace Phoenix.Bot.Utilities.Dialogs
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Phoenix.Bot.Utilities.Dialogs.Prompts;
+
+namespace Phoenix.Bot.Utilities.Dialogs
 {
     public static class PromptNames
     {
-        public const string Phone = "Phone_NumberPrompt";
-        public const string Code = "Code_NumberPrompt";
+        public const string Phone = nameof(Phone) + nameof(NumberPrompt<int>); // generic type T is not included
+        public const string Code = nameof(Code) + nameof(NumberPrompt<int>);
+        public const string HiddenChoices = nameof(HiddenChoices) + nameof(UnaccentedChoicePrompt);
     }
 }
