10c3cf66627b607d10e369c618d5d76b9adaace8
Théophile Spyrou <alfatheo21@gmail.com>
2021-03-07 16:31:32 +0200

    Fix Home Options

--- Phoenix.Bot.Utilities/State/Options/HomeOptions.cs
+++ Phoenix.Bot.Utilities/State/Options/HomeOptions.cs
@@ -1,9 +1,16 @@
 ﻿using Phoenix.Bot.Utilities.Actions;
+using Phoenix.DataHandle.Main;
 
 namespace Phoenix.Bot.Utilities.State.Options
 {
     public class HomeOptions : UserOptions
     {
         public BotAction Action { get; set; }
+
+        public HomeOptions(int userId, Role userRole)
+             : base(userId, userRole) { }
+
+        public HomeOptions(UserOptions userOptions)
+            : this(userOptions.UserId, userOptions.UserRole) { }
     }
 }
