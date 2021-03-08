9b9a080108b3fa4578b0f20fc4641f246aba6198
Théophile Spyrou <alfatheo21@gmail.com>
2021-03-08 14:19:04 +0200

    Update Calendar Extensions

--- Phoenix.Bot.Utilities/Dialogs/DialogsHelper.cs
+++ Phoenix.Bot.Utilities/Dialogs/DialogsHelper.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using Phoenix.Bot.Utilities.Linguistic;
-using Phoenix.Bot.Utilities.Miscellaneous;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -12,27 +8,6 @@ namespace Phoenix.Bot.Utilities.Dialogs
 {
     public static class DialogsHelper
     {
-        public static DateTime ResolveDateTime(IList<DateTimeResolution> dateTimeResolution)
-        {
-            if (dateTimeResolution == null || dateTimeResolution.Count == 0)
-                throw new Exception("Date Time Resolution cannot be null or empty.");
-            if (dateTimeResolution.Count == 1)
-                return DateTime.Parse(dateTimeResolution.Single().Value);
-
-            // The result gives 2 Dates with different year.
-            // If the date is past for the current year, then the results are (1) for the current year and (2) for the previous one.
-            // If the date is not past for the current year, then the results are (1) for the previous year and (2) for the current one.
-
-            var dateTimes = dateTimeResolution.Select(r => DateTime.Parse(r.Value)).OrderBy(d => d.Year);
-            var grDateTime = CalendarExtensions.GreeceLocalTime();
-
-            //bool isPastDate = dateTimes.All(d => d.Month < grDateTime.Month || (d.Month == grDateTime.Month && d.Day == grDateTime.Day));
-            //return isPastDate ? dateTimes.FirstOrDefault(d => d.Year == grDateTime.Year) : dateTimes.LastOrDefault();
-
-            // Just return the closest date including its year.
-            return dateTimes.Aggregate((d, cd) => Math.Abs((d - grDateTime).Days) < Math.Abs((cd - grDateTime).Days) ? d : cd);
-        }
-
         public static async Task<string> CreateGifUrlAsync(string rating, string query, int limit, int? offset, string key)
         {
             string giphyUrl = "http://api.giphy.com/v1/gifs/search" + $"?rating={rating}&q={query}&limit={limit}&offset={offset}&api_key={key}";
--- Phoenix.Bot.Utilities/Miscellaneous/CalendarExtensions.cs
+++ Phoenix.Bot.Utilities/Miscellaneous/CalendarExtensions.cs
@@ -1,9 +1,32 @@
-﻿using System;
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Phoenix.Bot.Utilities.Miscellaneous
 {
     public static class CalendarExtensions
     {
+        public static DateTimeOffset ResolveDateTime(IList<DateTimeResolution> dateTimeResolution)
+        {
+            if (dateTimeResolution == null || dateTimeResolution.Count == 0)
+                throw new ArgumentNullException("Date Time Resolution cannot be null or empty.");
+            if (dateTimeResolution.Count == 1)
+                return DateTimeOffset.Parse(dateTimeResolution.Single().Value);
+
+            // The result gives 2 Dates with different year.
+            // If the date is past for the current year, then the results are (1) for the current year and (2) for the previous one.
+            // If the date is not past for the current year, then the results are (1) for the previous year and (2) for the current one.
+
+            var dateTimes = dateTimeResolution.Select(r => DateTimeOffset.Parse(r.Value)).OrderBy(d => d.Year);
+
+            //bool isPastDate = dateTimes.All(d => d.Month < grDateTime.Month || (d.Month == grDateTime.Month && d.Day == grDateTime.Day));
+            //return isPastDate ? dateTimes.FirstOrDefault(d => d.Year == grDateTime.Year) : dateTimes.LastOrDefault();
+
+            // Just return the closest date including its year.
+            return dateTimes.Aggregate((d, cd) => Math.Abs((d - DateTimeOffset.UtcNow).Days) < Math.Abs((cd - DateTimeOffset.UtcNow).Days) ? d : cd);
+        }
+
         public static DateTime GreeceLocalTime()
                => TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("E. Europe Standard Time"));
     }
