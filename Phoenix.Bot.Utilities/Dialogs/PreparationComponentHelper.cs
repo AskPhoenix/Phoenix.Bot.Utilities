9b207d38cee71d7fdd07ff37ae2b7ecc20a5c7bd
Théophile Spyrou <alfatheo21@gmail.com>
2021-03-14 00:41:25 +0200

    Update Preparation Component Helper

--- Phoenix.Bot.Utilities/Dialogs/PreparationComponentHelper.cs
+++ Phoenix.Bot.Utilities/Dialogs/PreparationComponentHelper.cs
@@ -1,5 +1,4 @@
-﻿using Phoenix.Bot.Utilities.State.Options.Actions;
-using Phoenix.DataHandle.Main;
+﻿using Phoenix.DataHandle.Main;
 using Phoenix.DataHandle.Main.Models;
 using System;
 using System.Collections.Generic;
@@ -34,11 +33,53 @@ namespace Phoenix.Bot.Utilities.Dialogs
                 ToDictionary(l => l.Id, l => l.StartDateTime.ToString(dateFormat));
         }
 
+        public static Dictionary<int, string> GetDateSelectables(Course course, LectureTimeline? timeline = null, int daysNum = 5, string dateFormat = "d/M")
+        {
+            return FindClosestLectureDates(course, timeline ?? LectureTimeline.Past, daysNum, dateFormat);
+        }
+
+        public static Dictionary<int, string> GetDateSelectables(AspNetUsers user, LectureTimeline? timeline = null, int daysNum = 5, string dateFormat = "d/M")
+        {
+            var courses = user.TeacherCourse.Select(tc => tc.Course);
+            var tore = new Dictionary<int, string>(5 * courses.Count());
+
+            foreach (var course in courses)
+            {
+                var tempDict = FindClosestLectureDates(course, timeline ?? LectureTimeline.Anytime, daysNum, dateFormat);
+                if (tempDict != null)
+                    foreach (var lectureDatePair in tempDict)
+                        tore.Add(lectureDatePair.Key, lectureDatePair.Value);
+            }
+
+            return tore;
+        }
+
         public static Dictionary<int, string> FindLectureTimes(Course course, DateTimeOffset date)
         {
             return course.Lecture?.
                     Where(l => l.StartDateTime.Date == date.Date && l.Status == LectureStatus.Scheduled).
                     ToDictionary(l => l.Id, l => l.StartDateTime.ToString("t"));
+        }
+
+        public static Dictionary<int, string> GetLectureSelectables(Course course, DateTimeOffset dateToPrepareFor)
+        {
+            return PreparationComponentHelper.FindLectureTimes(course, dateToPrepareFor);
+        }
+
+        public static Dictionary<int, string> GetLectureSelectables(AspNetUsers user, DateTimeOffset dateToPrepareFor)
+        {
+            var courses = user.TeacherCourse.Select(tc => tc.Course);
+            var tore = new Dictionary<int, string>();
+
+            foreach (var course in courses)
+            {
+                var tempDict = PreparationComponentHelper.FindLectureTimes(course, dateToPrepareFor);
+                if (tempDict != null)
+                    foreach (var lectureTimePair in tempDict)
+                        tore.Add(lectureTimePair.Key, lectureTimePair.Value);
+            }
+
+            return tore;
         }
     }
 }
