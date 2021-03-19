3225727fa6ffe1037b9a2c21a140c46e28b8828a
Théophile Spyrou <alfatheo21@gmail.com>
2021-03-19 22:36:24 +0200

    Update Authentication Options for Affiliated Users authentication

--- Phoenix.Bot.Utilities/State/Options/AuthenticationOptions.cs
+++ Phoenix.Bot.Utilities/State/Options/AuthenticationOptions.cs
@@ -1,11 +1,17 @@
-﻿namespace Phoenix.Bot.Utilities.State.Options
+﻿using System;
+using System.Collections.Generic;
+
+namespace Phoenix.Bot.Utilities.State.Options
 {
     public class AuthenticationOptions
     {
         public bool IsOwnerVerification { get; set; }
         public string Phone { get; set; }
-        public string[] Codes { get; set; }
+        public Dictionary<int, string> Codes { get; set; }
+        public Dictionary<int, DateTimeOffset?> CodesCreatedAt { get; set; }
         public bool Verified { get; set; }
         public string VerifiedCode { get; set; }
+
+        public const int CodeDuration = 5;
     }
 }
