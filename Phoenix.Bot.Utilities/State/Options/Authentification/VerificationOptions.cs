918801daddd16a2d126c130ec1acb08115ba895a
Théophile Spyrou <alfatheo21@gmail.com>
2021-03-30 17:15:13 +0300

    Update Authentication related Options (Credentials and Verification Options)

---
+++ Phoenix.Bot.Utilities/State/Options/Authentification/VerificationOptions.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Phoenix.Bot.Utilities.State.Options.Authentification
+{
+    public class VerificationOptions : CredentialsOptions
+    {
+        public string Pin { get; set; }
+
+        public VerificationOptions(CredentialsOptions credentialsOptions)
+            : this(credentialsOptions.Codes, credentialsOptions.CodesCreatedAt)
+        {
+            this.Phone = credentialsOptions.Phone;
+            this.IsOwnerAuthentication = credentialsOptions.IsOwnerAuthentication;
+        }
+
+        [JsonConstructor]
+        private VerificationOptions(Dictionary<string, int> codes, Dictionary<string, DateTimeOffset> codesCreatedAt)
+            : base(codes, codesCreatedAt) { }
+    }
+}
