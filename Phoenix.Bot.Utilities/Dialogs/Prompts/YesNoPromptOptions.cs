4790b7565cf35a54b2fcfe8b754e6cef427d69b4
Théophile Spyrou <alfatheo21@gmail.com>
2021-03-31 21:19:38 +0300

    Update YesNo Prompt Options

--- Phoenix.Bot.Utilities/Dialogs/Prompts/YesNoPromptOptions.cs
+++ Phoenix.Bot.Utilities/Dialogs/Prompts/YesNoPromptOptions.cs
@@ -1,23 +1,33 @@
 ﻿using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Choices;
+using Phoenix.Bot.Utilities.Linguistic;
 using System.Collections.Generic;
 
 namespace Phoenix.Bot.Utilities.Dialogs.Prompts
 {
     public class YesNoPromptOptions : PromptOptions
     {
-        public YesNoPromptOptions() : base()
+        public const string Yes = "✔️ Ναι";
+        public const string No = "❌ Όχι";
+        public const string NoThanks = "❌ Όχι, ευχαριστώ";
+
+        public YesNoPromptOptions()
+            : this(false) { }
+
+        public YesNoPromptOptions(bool simpleNo) 
+            : base()
         {
             this.RetryPrompt = MessageFactory.Text("Παρακαλώ απάντησε με ένα Ναι ή Όχι:");
             this.Choices = new Choice[2] 
             {
-                new Choice("✔️ Ναι"),
-                new Choice("❌ Όχι, ευχαριστώ") { Synonyms = new List<string>(1) { "Όχι" } }
+                new Choice(Yes),
+                new Choice(simpleNo ? No : NoThanks) { Synonyms = new List<string>(1) { No.RemoveEmojis() } }
             };
         }
 
-        public YesNoPromptOptions(string promptText) : this()
+        public YesNoPromptOptions(string promptText, bool simpleNo = false)
+            : this(simpleNo)
         {
             this.Prompt = MessageFactory.Text(promptText);
         }
