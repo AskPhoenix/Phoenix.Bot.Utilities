8b5618d1c77148670cba0d3bc44364e7ebce5f4d
Théophile Spyrou <theofilos.spyrou@gmail.com>
2021-08-29 18:00:18 +0300

    Create Waterfall Names and Options for Broadcast Action

---
+++ Phoenix.Bot.Utilities/State/Options/Actions/BroadcastOptions.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Phoenix.DataHandle.Main;
+
+namespace Phoenix.Bot.Utilities.State.Options.Actions
+{
+    public class BroadcastOptions : ActionOptions
+    {
+        public string Message { get; set; }
+        public BroadcastAudience Audience { get; set; }
+        public BroadcastVisibility Visibility { get; set; }
+        public int? GroupCourseId { get; set; }
+
+        public enum BroadcastAudience
+        {
+            None = 0,
+            Students,
+            Parents,
+            StudentsParents,
+            Staff,
+            All
+        }
+
+        public enum BroadcastVisibility
+        {
+            Group = 0,
+            Global
+        }
+
+        public BroadcastOptions(ActionOptions actionOptions)
+            : base(actionOptions) { }
+
+        [JsonConstructor]
+        public BroadcastOptions(int userId, Role userRole)
+            : base(userId, userRole) { }
+
+        public BroadcastOptions(UserOptions userOptions)
+            : base(userOptions) { }
+    }
+}
