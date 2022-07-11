5e60f3afad8b02b0d0d47987a5c832695889daa8
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-07-11 15:44:26 +0200

    Create Waterfall Extensions

---
+++ Phoenix.Bot.Utilities/Dialogs/WaterfallExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Phoenix.DataHandle.Main.Types;
+
+namespace Phoenix.Bot.Utilities.Dialogs
+{
+    public static class WaterfallExtensions
+    {
+        public static ChannelProvider GetProvider(this WaterfallStepContext stepContext)
+        {
+            return stepContext.Context.Activity.ChannelId.ToChannelProvider();
+        }
+
+        public static string GetProviderKey(this WaterfallStepContext stepContext)
+        {
+            return stepContext.Context.Activity.From.Id;
+        }
+
+        public static string GetRecipientKey(this WaterfallStepContext stepContext)
+        {
+            return stepContext.Context.Activity.Recipient.Id;
+        }
+    }
+}
