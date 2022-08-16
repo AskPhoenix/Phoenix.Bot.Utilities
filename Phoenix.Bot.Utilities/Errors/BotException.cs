94e7506c8cb00c9752c13e9c0ac3b7496a371978
Théophile Spyrou <theofilos.spyrou@gmail.com>
2022-08-16 16:46:33 +0300

    Update Bot Exception

--- Phoenix.Bot.Utilities/Errors/BotException.cs
+++ Phoenix.Bot.Utilities/Errors/BotException.cs
@@ -5,18 +5,20 @@
         public BotError Error { get; }
         public string Solution { get; }
         public string Code { get; }
+        public bool ShowMessageOnly { get; set; }
 
         public BotException()
             : this(BotError.Unknown)
         {
         }
 
-        public BotException(BotError botError)
+        public BotException(BotError botError, bool showMessageOnly = false)
             : base($"{botError.GetMessage()}")
         {
             this.Error = botError;
             this.Solution = botError.GetSolution();
             this.Code = ((int)botError).ToString("0000");
+            this.ShowMessageOnly = showMessageOnly;
         }
     }
 }
